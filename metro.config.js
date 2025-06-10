15a61123b5bdf313f46f64953ba37aca6fbc5e5e
Arda Erturk <arda.erturk@mail.utoronto.ca>
2025-06-10 18:59:29 -0400

    minor improvements

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,9 @@ const { getDefaultConfig } = require("expo/metro-config");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.resolver.extraNodeModules = {
+  crypto: require.resolve('react-native-quick-crypto'),
+  'node:crypto': require.resolve('react-native-quick-crypto'),
+};
+
 module.exports = config;
